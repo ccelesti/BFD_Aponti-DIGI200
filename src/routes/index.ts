e4686be1b8e7431707b8dcd257fcb600b21f0574
Gabriel Xavier <gabrielluck492@gmail.com>
2026-01-03 16:17:43 -0300

    feat: adição de controllers e routes de fornecedor

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,24 @@
 import { Router } from "express";
-import { healthCheck } from "../controllers";
-import { adicionarClientes, listarClientes, listarCliente, editarCliente, excluirCliente } from "../controllers";
-
+import * as cliente from "../controllers/index";
+import * as fornecedor from "../controllers/fornecedor";
 export const router = Router();
 
 // Healthcheck da API
-router.get("/healthcheck", healthCheck);
+router.get("/healthcheck", cliente.healthCheck);
 
 // Clientes
-router.post("/clientes", adicionarClientes);
-router.get("/clientes", listarClientes);
-router.get("/clientes/:id", listarCliente);
-router.put("/clientes/:id", editarCliente);
-router.delete("/clientes/:id", excluirCliente);
+router.post("/clientes", cliente.adicionarClientes);
+router.get("/clientes", cliente.listarClientes);
+router.get("/clientes/:id", cliente.listarCliente);
+router.put("/clientes/:id", cliente.editarCliente);
+router.delete("/clientes/:id", cliente.excluirCliente);
+
+// Fornecedor
+router.post("/fornecedor", fornecedor.adicionarFornecedor);
+router.get("/fornecedores", fornecedor.listarFornecedor);
+router.get("/fornecedores/:id", fornecedor.listarFornecedores);
+router.put("/fornecedores/:id", fornecedor.editarFornecedor);
+router.delete("/fornecedores/:id", fornecedor.excluirFornecedor);
+
 
 export default router;
