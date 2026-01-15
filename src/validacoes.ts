f2725aad152dd6e342ee2ccb47f7625eb0221009
Ruberval Brasileiro <tecdsis40@gmail.com>
2026-01-15 13:49:19 -0300

    Implementação do código validações

---
+++ src/validacoes.ts
@@ -0,0 +1,66 @@
+// Interface para padronizar a resposta de erro
+interface ResultadoValidacao {
+  valido: boolean;
+  erros: string[];
+}
+
+// 1. Auxiliar: Remove tudo que não for número (limpa pontos, traços e barras)
+export class validador {
+private static apenasNumeros(valor: any): string{
+    if (typeof valor !== 'string') return "";
+    return valor.replace(/\D/g, '');
+}
+
+// 2. Validação de Texto Simples (Nome, Bairro, Endereço)
+private static validarTexto (campo: string, valor: any, min: number = 3): string | null {
+    if (typeof valor !== 'string') return `O Campo ${campo} deve ser um texto.`;
+    if (valor.trim().length < min) return `O Campo ${campo} deve ter no minímo ${min} Caracteres`;
+    return null;
+}
+
+// 3. Validação de CPF (11 dígitos)
+private static validarCPF(cpf: any): string | null {
+    const numeros = this.apenasNumeros(cpf);
+    if (numeros.length !== 11) return "O CPF deve ter exatamente 11 dígitos numéricos.";
+    return null;
+}
+
+// 4. Validação de CNPJ (14 dígitos)
+private static validarCNPJ(cnpj: any): string | null {
+    const numeros = this.apenasNumeros(cnpj);
+    if (numeros.length !== 14) return"O CNPJ deve conter exatamente 14 dígitos numéricos.";
+    return null;
+}
+
+// --- VALIDAÇÃO DE CLIENTE ---
+static validarCliente(dados: any): ResultadoValidacao {
+    const erros: string [] = [];
+    
+    const erroNome = this.validarTexto( "Nome do Cliente", dados.nome);
+    const erroCpf = this.validarCPF(dados.cpf);
+    const erroEndereco = this.validarTexto("Endereço", dados.endereco, 5);
+    const erroBairro = this.validarTexto("Bairro", dados.bairro, 2);
+
+    if (erroNome) erros.push(erroNome);
+    if (erroCpf) erros.push(erroCpf);
+    if (erroEndereco) erros.push(erroEndereco);
+    if (erroBairro) erros.push(erroBairro);
+
+    return {valido: erros.length === 0, erros };
+}
+
+// --- VALIDAÇÃO DE FORNECEDOR ---
+static validarFornecedor(dados: any): ResultadoValidacao {
+    const erros: string[] = [];
+
+    const erroNomeFornecedor = this.validarTexto("O Nome do Fornecedor", dados.nome);
+    const erroCnpj = this.validarCNPJ("O CNPJ", dados.cnpj);
+    const erroEnderecoFornecedor = this.validarTexto("O Endereço", dados.endereco, 5);
+
+    return {valido: erros.length ===0, erros};
+}
+
+}
+/*
+const teste = Validador.validarFornecedor({ nome: "Empresa X", cnpj: "123", endereco: "Rua" });
+console.log(teste); // Deve retornar valido: false e a lista de erros de dígitos e tamanho.
