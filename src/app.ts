4809ba5bf9c12d4752efae2c8f273d4bfd3cb023
Ruberval Brasileiro <tecdsis40@gmail.com>
2026-01-16 17:08:23 -0300

    Refinamento código de laidações forcedor e cliente teste

--- src/app.ts
+++ src/app.ts
@@ -15,11 +15,25 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-console.log('validacoes.ts carregado!\n');
+//console.log('validacoes.ts carregado!\n');
 
+console.log(Validador.validarCliente({
+  nome: 'Rubinho',
+  cpf: '123.456.789-11',
+  endereco: 'Rua da amora',
+  bairro: 'Rio Doce'
+}));
+console.log()
 console.log(Validador.validarFornecedor({
-  nome: `Aqui tem X`,
-  cnpj: `123456789`,
-  endereco: 'Rua Destino'
+  razaoSocial: 'Almir Slan',
+  nome: `Casas Bahia`,
+  cnpj: `62.326.348/0001-70`,
+  email: 'contato@bahia.com',
+  whatsapp: '81996335654',
+  telefone: '81934329781',
+  rua: 'Rua Do Conhecimento',
+  bairro: 'Casa Amarela',
+  cidade: 'Olinda',
+  senha: '123fgtr'
 }));
 
--- src/validacoes.ts
+++ src/validacoes.ts
@@ -1,5 +1,5 @@
 // Interface para padronizar a resposta de erro
-console.log('\nvalidacoes.ts carregado!');
+console.log('\nvalidacoes.ts carregado!\n');
 interface ResultadoValidacao {
   valido: boolean;
   erros: string[];
@@ -8,14 +8,15 @@ interface ResultadoValidacao {
 // 1. Auxiliar: Remove tudo que não for número (limpa pontos, traços e barras)
 export class Validador {
 private static apenasNumeros(valor: any): string{
-    console.log('\nvalidacoes.ts carregado!');
+   // console.log('\nvalidacoes.ts carregado!');
     if (typeof valor !== 'string') return "";
     return valor.replace(/\D/g, '');
 }
 
 // 2. Validação de Texto Simples (Nome, Bairro, Endereço)
 private static validarTexto (campo: string, valor: any, min: number = 3): string | null {
-    console.log(`validando texto: "${campo}" = "${valor}"`);
+    const valorExibido = campo.toLowerCase().includes('senha') ? '********' : valor;
+    console.log(`validando texto: "${campo}" = "${valorExibido}"`);
     if (typeof valor !== 'string') return `O Campo ${campo} deve ser um texto.`;
     if (valor.trim().length < min) return `O Campo ${campo} deve ter no minímo ${min} Caracteres`;
     return null;
@@ -23,19 +24,24 @@ private static validarTexto (campo: string, valor: any, min: number = 3): string
 
 // 3. Validação de CPF (11 dígitos)
 private static validarCPF(cpf: any): string | null {
-    console.log('validando CPF:', cpf) ?? '';
+    console.log('validando CPF:', cpf);
     const numeros = this.apenasNumeros(cpf);
-    if (numeros.length !== 11) return "O CPF deve ter exatamente 11 dígitos numéricos.";
-    return null;
+    console.log('Números CPF:', numeros, 'length', numeros.length);
+    if (numeros.length !== 11) {
+         console.log('Erro CPF:', 'Deve ter 11 dígitos'); 
+        return 'O CPF deve ter exatamente 11 dígitos numéricos.';
+    }return null;
 }
 
-// 4. Validação de CNPJ (14 dígitos)
+// 4 Validação de CNPJ (14 dígitos)
 private static validarCNPJ(cnpj: any): string | null {
     console.log('validando CNPJ:', cnpj);
-    const numeros = this.apenasNumeros(cnpj ?? '');
-   // console.log('números CNPJ:', numeros, 'length:', numeros.length);
-    if (numeros.length !== 14) return "O CNPJ deve conter exatamente 14 dígitos numéricos.";
-    return null;
+    const numeros = this.apenasNumeros(cnpj);
+    console.log('números CNPJ:', numeros, 'length:', numeros.length);
+    if (numeros.length !== 14) {
+        console.log('ERRO CNPJ:', 'Deve ter 14 dígitos');
+        return 'O CNPJ deve conter exatamente 14 dígitos numéricos.';
+    }return null;
 }
 
 // --- VALIDAÇÃO DE CLIENTE ---
@@ -55,19 +61,54 @@ static validarCliente(dados: any): ResultadoValidacao {
     return {valido: erros.length === 0, erros };
 }
 
-
 // --- VALIDAÇÃO DE FORNECEDOR ---
 static validarFornecedor(dados: any): ResultadoValidacao {
     const erros: string[] = [];
 
     const erroNomeFornecedor = this.validarTexto("O Nome do Fornecedor", dados.nome);
-    const erroCnpj = this.validarCNPJ(dados.cnpj);
-    const erroEnderecoFornecedor = this.validarTexto("O Endereço", dados.endereco, 5);
+    if (erroNomeFornecedor) erros.push(erroNomeFornecedor);
 
+    const erroCnpj = this.validarCNPJ(dados.cnpj);
+    if (erroCnpj) erros.push(erroCnpj);
+
+    const erroRazao = this.validarTexto('Razão Social', dados.razaoSocial);
+    if (erroRazao) erros.push(erroRazao);
+
+    const erroEmail = this.validarTexto('E-mail', dados.email);
+    if (erroEmail) erros.push(erroEmail);
+
+    const erroWhatsapp = this.validarTelefone('Whatsapp',dados.whatsapp);
+    if (erroWhatsapp) erros.push(erroWhatsapp);
+
+    const erroTelefone = this.validarTelefone('Telefone',dados.telefone);
+    if (erroTelefone) erros.push(erroTelefone);
+
+    const erroRua = this.validarTexto('Rua', dados.rua);
+    if (erroRua) erros.push(erroRua);
+
+    const erroBairro = this.validarTexto('Bairro', dados.bairro, 2);
+    if (erroBairro) erros.push(erroBairro);
+
+    const erroCidade = this.validarTexto('Cidade', dados.cidade);
+    if (erroCidade) erros.push(erroCidade);
+
+    const erroSenha = this.validarTexto('Senha', dados.senha)
+    if (erroSenha) erros.push(erroSenha);
     return {valido: erros.length ===0, erros};
 }
 
+// 5. Validação de Telefone/WhatsApp (DDD + 9 dígitos = 11 números)
+private static validarTelefone(campo: string, valor: any): string | null {
+    if(!valor) return `O campo ${campo} é obrigatório.`;
+    
+    // Limpa a string deixando apenas números
+    const numeros = this.apenasNumeros(valor);
+    //console.log(`validando ${campo}: "${valor}" -> números: ${numeros} (tamanho: ${numeros.length})`);
+    
+    console.log(`validando campo ${campo}:`, valor);
+    if (numeros.length !== 11) {
+        return `O campo ${campo} deve ter exatamente 11 dígitos (DDD + número).`;
+    }return null;
 }
-/*
-const teste = Validador.validarFornecedor({ nome: "Empresa X", cnpj: "123", endereco: "Rua" });
-console.log(Validador); // Deve retornar valido: false e a lista de erros de dígitos e tamanho. */
+
+}
